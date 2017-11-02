4d60c5f49bb979ac7af341c5eefc3f11d8baa7a6
root <root@sk>
2017-11-02 10:22:06 +0800

    C01 finished

---
+++ C01/L02_序列/l01_元祖.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-11-01 16:51:45;  @author: joinpython.com
+'''
+
+a=("join","python",1)
+print type(a)
+
---
+++ C01/L02_序列/l01_列表.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-11-02 09:58:50;  @author: joinpython.com
+'''
